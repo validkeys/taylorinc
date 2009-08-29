12202edf4d776ca77460a3e5f5897304dfa91c19
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2009-08-29 19:21:43 -0400

    cleaned up admin forms and lists

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ActiveRecord::Base
+  
+  # consolidates address feilds
+  def address
+    "#{street_1} #{street_2}, #{city}, #{province}, #{country}, #{postal_code}"
+  end
+  
 end
