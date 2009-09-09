a4416ab694de8411c5b7953c964a0aa787c5de6c
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2009-09-09 15:49:12 -0400

    changed friendly_id from gem to plugin

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'friendly_id', :source => 'http://gems.github.com'
+  # config.gem 'friendly_id', :source => 'http://gems.github.com'
   config.gem 'RedCloth'
   #config.gem 'resource_controller'
 
