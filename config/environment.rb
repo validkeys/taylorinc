d46538337b5594b40d2cc7afd84252ff187584ad
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2009-09-09 12:48:49 -0400

    fixed paperclip options

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,4 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  
-  Paperclip.option[:log_command] = true
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,2 @@
 Paperclip.options[:image_magick_path] = IMAGE_MAGICK_PATH
+Paperclip.options[:log_command] = true
