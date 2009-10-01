4632fde3a17677bd4f680b69eda3d26ddc344a26
Dave Rapin <daverapin@dave-rapins-macbook-pro.local>
2009-10-01 09:14:39 -0400

    working ie header xml fix implemented

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,9 +3,6 @@ class LocationsController < ApplicationController
   
   actions :index, :show # only handle index and show actions
   
-  index.wants.xml { render  :template => "locations/index.xml" }
-  show.wants.xml { render  :template => "locations/show.xml" }
-  
   index.wants.html { 
     redirect_to location_path(Location.find(:first, :order => 'position'))
   }
@@ -13,96 +10,8 @@ class LocationsController < ApplicationController
   show.wants.html {
     @locations = Location.find :all, :order => 'position'
   }
+  
+  index.wants.xml { render  :template => "locations/index.xml" }
+  show.wants.xml { render  :template => "locations/show.xml" }
+  
 end
-
-
-# --------------
-# Actions replaced by resource_controller plugin
-# Leaving them in here for reference
-#---------------
-=begin
-
-  # GET /locations
-  # GET /locations.xml
-  def index
-    @locations = Location.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @locations }
-    end
-  end
-
-  # GET /locations/1
-  # GET /locations/1.xml
-  def show
-    @location = Location.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @location }
-    end
-  end
-
-  # GET /locations/new
-  # GET /locations/new.xml
-  def new
-    @location = Location.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @location }
-    end
-  end
-
-  # GET /locations/1/edit
-  def edit
-    @location = Location.find(params[:id])
-  end
-
-  # POST /locations
-  # POST /locations.xml
-  def create
-    @location = Location.new(params[:location])
-
-    respond_to do |format|
-      if @location.save
-        flash[:notice] = 'Location was successfully created.'
-        format.html { redirect_to(@location) }
-        format.xml  { render :xml => @location, :status => :created, :location => @location }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /locations/1
-  # PUT /locations/1.xml
-  def update
-    @location = Location.find(params[:id])
-
-    respond_to do |format|
-      if @location.update_attributes(params[:location])
-        flash[:notice] = 'Location was successfully updated.'
-        format.html { redirect_to(@location) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /locations/1
-  # DELETE /locations/1.xml
-  def destroy
-    @location = Location.find(params[:id])
-    @location.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(locations_url) }
-      format.xml  { head :ok }
-    end
-  end
-=end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,95 +3,9 @@ class SectionsController < ApplicationController
   
   actions :index, :show #only handle index and show actions
   
+  show.wants.html { redirect_to section_page_path(@section, @section.pages.first) }
+  
   index.wants.xml { render  :template => "sections/index.xml" }
   show.wants.xml { render  :template => "sections/show.xml" }
   
-  show.wants.html { redirect_to section_page_path(@section, @section.pages.first) }
 end
-
-
-=begin
-  # GET /sections
-  # GET /sections.xml
-  def index
-    @sections = Section.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @sections }
-    end
-  end
-
-  # GET /sections/1
-  # GET /sections/1.xml
-  def show
-    @section = Section.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @section }
-    end
-  end
-
-  # GET /sections/new
-  # GET /sections/new.xml
-  def new
-    @section = Section.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @section }
-    end
-  end
-
-  # GET /sections/1/edit
-  def edit
-    @section = Section.find(params[:id])
-  end
-
-  # POST /sections
-  # POST /sections.xml
-  def create
-    @section = Section.new(params[:section])
-
-    respond_to do |format|
-      if @section.save
-        flash[:notice] = 'Section was successfully created.'
-        format.html { redirect_to(@section) }
-        format.xml  { render :xml => @section, :status => :created, :location => @section }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @section.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /sections/1
-  # PUT /sections/1.xml
-  def update
-    @section = Section.find(params[:id])
-
-    respond_to do |format|
-      if @section.update_attributes(params[:section])
-        flash[:notice] = 'Section was successfully updated.'
-        format.html { redirect_to(@section) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @section.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /sections/1
-  # DELETE /sections/1.xml
-  def destroy
-    @section = Section.find(params[:id])
-    @section.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(sections_url) }
-      format.xml  { head :ok }
-    end
-  end
-=end
